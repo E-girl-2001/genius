44e489119a574b76856faef8abca8b124a3684de
E-girl-2001 <lri35@uclive.ac.nz>
2024-03-06 12:48:09 +1300

    not much

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from classes import Beverage
 from liquorland import *
 from write_to_csv import *
 
-filename = "drinks2.csv"
-beverage_types = ["ready-to-drink", "wine", "spirits", "craft beer", "beer"]
+filename = "drinks.csv"
+beverage_types = ["ready-to-drink", "wine", "spirits", "craft-beer", "beer"]
 
 
 init_csv(filename)
-get_drinks_data("ready-to-drink", filename)
+get_drinks_data("craft-beer", filename)
 
 
 
